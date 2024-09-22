1c2ccc2f5439db0e124ea76e43db9d7546d710cd
sehwan505 <sehwan505@gmail.com>
2024-09-22 18:00:47 +0900

    Feat: remove temp_dir after changing work dir

--- mafm/fileops.py
+++ mafm/fileops.py
@@ -2,30 +2,40 @@ import ctypes
 
 lib = ctypes.CDLL("./C_library/libfileops.so")
 
-lib.make_soft_links.argtypes = [ctypes.POINTER(ctypes.c_char_p), ctypes.c_int]
+lib.make_soft_links.argtypes = [
+    ctypes.POINTER(ctypes.c_char_p),
+    ctypes.c_int,
+    ctypes.c_char_p,
+]
 lib.make_soft_links.restype = ctypes.c_char_p
 
 
-def make_soft_links(paths):
+def make_soft_links(paths, temp_dir):
     path_array = (ctypes.c_char_p * len(paths))(
         *[path.encode("utf-8") for path in paths]
     )
-    result = lib.make_soft_links(path_array, len(paths))
+    result = lib.make_soft_links(path_array, len(paths), temp_dir.name.encode("utf-8"))
     return result.decode("utf-8")
 
 
-lib.get_file_data.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_int)]
-lib.get_file_data.restype = ctypes.c_char_p
+lib.get_file_data.argtypes = [ctypes.c_char_p]
+lib.get_file_data.restype = ctypes.POINTER(ctypes.c_char_p)
 
 
 def get_file_data(path):
-    is_image_or_video = ctypes.c_int(0)
-    result = lib.get_file_data(path.encode("utf-8"), ctypes.byref(is_image_or_video))
-    return result.decode("utf-8"), bool(is_image_or_video.value)
+    result = lib.get_file_data(path.encode("utf-8"))
+    data_list = []
+    idx = 0
+
+    while result[idx] is not None:
+        string = ctypes.string_at(result[idx]).decode("utf-8")
+        data_list.append(string)
+        idx += 1
+    return data_list
 
 
 lib.get_all_file_data.argtypes = [ctypes.c_char_p, ctypes.POINTER(ctypes.c_int)]
-lib.get_all_file_data.restype = ctypes.POINTER(ctypes.c_char_p)
+lib.get_all_file_data.restype = ctypes.POINTER(ctypes.POINTER(ctypes.c_char_p))
 
 
 def get_all_file_data(directory):
@@ -33,5 +43,16 @@ def get_all_file_data(directory):
     result = lib.get_all_file_data(directory.encode("utf-8"), ctypes.byref(num_files))
     files = []
     for i in range(num_files.value):
-        files.append(result[i].decode("utf-8"))
+        idx = 0
+        data_list = []
+        print(result[i])
+        while result[i][idx] is not None:
+            # print(ctypes.string_at(result[i][idx]))
+            try:
+                string = ctypes.string_at(result[i][idx]).decode("utf-8")
+            except:
+                string = ctypes.string_at(result[i][idx])
+            data_list.append(string)
+            idx += 1
+        files.append(data_list)
     return files
--- mafm/shell.py
+++ mafm/shell.py
@@ -1,22 +1,29 @@
 import os
 import subprocess
+import tempfile
 from fileops import make_soft_links, get_file_data, get_all_file_data
 
+link_dir = None
 
-def execute_command(command, link_dir):
+
+def execute_command(command):
+    global link_dir
     try:
         cmd_parts = command.strip().split()
         if cmd_parts[0] == "mf":
-            # file_data, is_image_or_video = get_file_data(cmd_parts[1])
+            # file_data = get_all_file_data(cmd_parts[1])
             # print(file_data)
-            temp_dir = make_soft_links(
+
+            temp_dir = tempfile.TemporaryDirectory()
+            make_soft_links(
                 [
                     "/Users/parksehwan/Documents/MAFM/mafm/shell.py",
                     "/Users/parksehwan/Documents/MAFM/mafm/a.txt",
-                ]
+                ],
+                temp_dir,
             )
-            os.chdir(temp_dir)
-            # link_dir = temp_dir
+            os.chdir(temp_dir.name)
+            link_dir = temp_dir
             return
         elif cmd_parts[0] == "cd":
             try:
@@ -38,12 +45,13 @@ def execute_command(command, link_dir):
 
 
 def shell():
-    link_dir = ""
+    global link_dir
+
     while True:
         cwd = os.getcwd()
-        # if not cwd.startswith(link_dir):
-        #     os.remove(link_dir)
-        #     link_dir = ""
+        if link_dir != None and not link_dir.name in cwd:
+            link_dir.cleanup()
+            link_dir = None
         command = input(f"{cwd} $ ")
 
         if command.strip().lower() in ["exit", "quit"]:
@@ -52,7 +60,7 @@ def shell():
         elif command.strip() == "":
             continue
         else:
-            execute_command(command, link_dir)
+            execute_command(command)
 
 
 if __name__ == "__main__":
