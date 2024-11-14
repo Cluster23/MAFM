7259ed7e17d4367203934e72dfff0c4fac2233a9
Jaewon <20011211s@gmail.com>
2024-11-14 09:53:22 +0900

    Feat: watchdog에서 modified 제거

--- mafm/observer.py
+++ mafm/observer.py
@@ -27,28 +27,30 @@ from collections import defaultdict
 class FileEventHandler(FileSystemEventHandler):
     """파일 시스템 이벤트 핸들러 클래스"""
 
-    def __init__(self, cooldown=1.0):
+    def __init__(self):
         super().__init__()
-        self.recent_events = defaultdict(float)  # 최근 이벤트 기록용 딕셔너리 (파일 경로별로 타임스탬프 저장)
-        self.cooldown = cooldown  # 이벤트 발생 쿨다운 시간 (초)
 
     def is_dot_file(self, path):
         """숨김 파일인지 확인하는 함수"""
         return os.path.basename(path).startswith(".")
 
+    def is_ignored_file(self, path):
+        """특정 패턴을 가진 파일을 무시하는 함수"""
+        ignored_patterns = ["db-journal",".db"]
+        return any(pattern in path for pattern in ignored_patterns)
+
     def on_deleted(self, event):
         """파일 또는 디렉토리 삭제 이벤트 처리 함수"""
-        if self.is_dot_file(event.src_path):
-            return  # 숨김 파일은 무시
+        if self.is_dot_file(event.src_path) or self.is_ignored_file(event.src_path):
+            print("ignore created: " + event.src_path)
+            return  # 숨김 파일과 무시할 패턴을 가진 파일은 무시
 
         print("--deleted--")
+        print("deleting: " + event.src_path)
 
         if event.is_directory:
             dir_path = event.src_path
             dir_name = os.path.basename(dir_path)
-
-            print("dir_path: " + dir_path)
-            print("dir_name: " + dir_name)
 
             db_name = dir_path + "/" + dir_name + ".db"
             delete_vector_db(db_name)  # 디렉토리와 연결된 벡터 DB 삭제
@@ -59,41 +61,31 @@ class FileEventHandler(FileSystemEventHandler):
         file_path = event.src_path
         dir_path = os.path.dirname(file_path)
 
-        print("dir_path: " + dir_path)
-        print("file_path: " + file_path)
-
         db_name = dir_path + "/" + os.path.basename(dir_path) + ".db"
         id = get_id_by_path(file_path, "filesystem.db")
         remove_by_id(id, db_name)  # 벡터 DB에서 파일 데이터 삭제
         print(f"Deleted file: {event.src_path}")
 
-    def on_modified(self, event):
-        print("on_modified")
-        """파일 수정 이벤트 처리 함수"""
-        if event.is_directory or self.is_dot_file(event.src_path):
-            return  # 디렉토리와 숨김 파일은 무시
-
-        # 최근 생성된 파일인지 확인하여 쿨다운 시간 이내에 발생한 수정 이벤트는 무시
-        if (time.time() - self.recent_events.get(event.src_path, 0)) < self.cooldown:
-            return  # 쿨다운 시간 이내이므로 무시
-
-        file_src_path = event.src_path
-        dir_path = os.path.dirname(file_src_path)
-        db_name = dir_path + "/" + os.path.basename(dir_path) + ".db"
-
-        print("--modified--")
-        print("dir_path: " + dir_path)
-        print("db_name: " + db_name)
-
-        id = get_id_by_path(file_src_path, "filesystem.db")
-        remove_by_id(id, db_name)  # 기존 벡터 데이터 제거
-        save(db_name, id, get_file_data(file_src_path)[2:])  # 새로운 벡터 데이터 저장
-        insert_file_info(file_src_path, 0, "filesystem.db")  # 파일 정보 DB 업데이트
-        print(f"Modified file: {event.src_path}")
+    # def on_modified(self, event):
+    #     """파일 수정 이벤트 처리 함수"""
+    #     if event.is_directory or self.is_dot_file(event.src_path) or self.is_ignored_file(event.src_path):
+    #         print("directory or dotfile modified")
+    #         return  # 디렉토리와 숨김 파일은 무시
+    #
+    #     file_src_path = event.src_path
+    #     dir_path = os.path.dirname(file_src_path)
+    #     db_name = dir_path + "/" + os.path.basename(dir_path) + ".db"
+    #
+    #
+    #     id = get_id_by_path(file_src_path, "filesystem.db")
+    #     remove_by_id(id, db_name)  # 기존 벡터 데이터 제거
+    #     save(db_name, id, get_file_data(file_src_path)[2:])  # 새로운 벡터 데이터 저장
+    #     insert_file_info(file_src_path, 0, "filesystem.db")  # 파일 정보 DB 업데이트
+    #     print(f"Modified file: {event.src_path}")
 
     def on_moved(self, event):
         """파일 또는 디렉토리 이동 이벤트 처리 함수"""
-        if self.is_dot_file(event.src_path) or self.is_dot_file(event.dest_path):
+        if self.is_dot_file(event.src_path) or self.is_dot_file(event.dest_path) or self.is_ignored_file(event.src_path) or self.is_ignored_file(event.dest_path):
             return  # 숨김 파일은 무시
 
         print("--moved--")
@@ -108,18 +100,16 @@ class FileEventHandler(FileSystemEventHandler):
     def on_created(self, event):
         print("--created--", flush=True)
         """파일 생성 이벤트 처리 함수"""
-        if self.is_dot_file(event.src_path):
-            return  # 숨김 파일은 무시
+        if self.is_dot_file(event.src_path) or self.is_ignored_file(event.src_path):
+            return  # 숨김 파일과 무시할 패턴을 가진 파일은 무시
 
-        self.recent_events[event.src_path] = time.time()  # 생성된 파일 기록
         absolute_file_path = event.src_path
         dirpath = os.path.dirname(absolute_file_path)
         dirname = os.path.basename(dirpath)
 
-        print("dirpath: " + dirpath, flush=True)
-        print("dirname: " + dirname, flush=True)
 
         if event.is_directory:
+            print("created directory")
             try:
                 initialize_vector_db(dirpath + "/" + dirname + ".db")  # 벡터 DB 초기화
                 id = insert_file_info(absolute_file_path, 1, "filesystem.db")
@@ -127,6 +117,7 @@ class FileEventHandler(FileSystemEventHandler):
             except Exception as e:
                 print(f"Error initializing vector DB for directory: {e}")
         else:
+            print("created file")
             insert_file_info(absolute_file_path, 0, "filesystem.db")  # 파일 정보 DB에 추가
             file_chunks = get_file_data(absolute_file_path)
             save(dirpath + "/" + dirname + ".db", get_id_by_path(absolute_file_path, "filesystem.db"), file_chunks[2:])
